8cd7f3c1ae8e014b2730353382dae0ba90e58368
Morulus <vladimirmorulus@gmail.com>
2014-10-10 18:21:44 +0300

    license + module

---
+++ jquery.tie.js
@@ -0,0 +1,49 @@
+/**
+UltimarteZoom - jQuery plugin for zooming images on web page.
+@author Vladimir Kalmykov (http://github. )
+*/
+(function(jQuery) {
+	var jQuery = jQuery;
+	jQuery.tie = jQuery.fn.tie = function(func) {
+		func.apply(this);
+		return this;
+	};
+
+	jQuery.under = jQuery.fn.under = function(obj) {
+		return jQuery(obj).prependTo(this);
+	};
+
+	jQuery.put = jQuery.fn.put = function(obj) {
+		return jQuery(obj).appendTo(this);
+	};
+
+	jQuery.and = jQuery.fn.and = function(obj) {
+		return jQuery(obj).insertAfter(this);
+	};
+
+	jQuery.condition = jQuery.fn.condition = function(condit, onTrue, onFalse) {
+		if (condit) {
+			if (typeof onTrue == 'function') return onTrue.call(this, condit);
+			return this;
+		} else {
+			if (typeof onFalse == 'function') return onFalse.call(this);
+			return this;
+		};
+	};
+
+	jQuery.ramp = jQuery.fn.ramp = function() {
+		var current = this;
+		for (var i=0;i<arguments.length;i++) {
+			var variable = arguments[i];
+			if (variable instanceof Array) {
+				for (var a=0;a<variable.length;a++) {
+					jQuery(current).put(variable[variable]);
+				};
+				current = jQuery(current).parent();
+			} else {
+				current = jQuery(current).put(arguments[i])
+			};
+		};
+		return current;
+	};
+})(jQuery)
